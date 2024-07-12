cffc2c896beaba38ebf609bb958291014cc4452a
Danial Irfan <developersdani@gmail.com>
2024-07-12 22:05:57 +0800

    update config
    - add environment variables

--- database/config.php
+++ database/config.php
@@ -1,8 +1,14 @@
 <?php
 
+$env = parse_ini_string(file_get_contents(__DIR__.'/.env'));
+$hostname = $env['HOSTNAME'];
+$username = $env['USERNAME'];
+$dbname = $env['DBNAME'];
+$password = $env['PASSWORD'];
+
 $SERVER = "localhost";
-$USER = "kvkualas_KVKSadmin";
-$PSWD = "Kvks@2023";
+$USER = "root";
+$PSWD = "danialdev";
 $DB = "kvkualas_hep";
 
 $con = mysqli_connect($SERVER, $USER, $PSWD, $DB);
