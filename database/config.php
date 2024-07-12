3ee7839258e59ff3f7c164ddf77e8d1efcd5437b
Danial Irfan <developersdani@gmail.com>
2024-07-12 22:13:29 +0800

    update config
    - fix .env

--- database/config.php
+++ database/config.php
@@ -1,14 +1,9 @@
 <?php
 
 $env = parse_ini_string(file_get_contents(__DIR__.'/.env'));
-$hostname = $env['HOSTNAME'];
-$username = $env['USERNAME'];
-$dbname = $env['DBNAME'];
-$password = $env['PASSWORD'];
-
-$SERVER = "localhost";
-$USER = "root";
-$PSWD = "danialdev";
-$DB = "kvkualas_hep";
+$SERVER = $env['HOSTNAME'];
+$USER = $env['USERNAME'];
+$PSWD = $env['PASSWORD'];
+$DB = $env['DBNAME'];
 
 $con = mysqli_connect($SERVER, $USER, $PSWD, $DB);
